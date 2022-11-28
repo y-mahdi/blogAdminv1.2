cc0e1ee94df00aede8a4701fa52f664a0fbda667
MAHDI YOUSSEF <you@example.com>
2022-11-28 08:59:21 +0100

    fin question 4

--- models/post/postModel.js
+++ models/post/postModel.js
@@ -4,18 +4,25 @@ const mongoose=require("mongoose")
 // definition d'un schèma
 const postSchema=mongoose.Schema({
     titre: {
-       // chaine de caractère unique non nulle
+        type:String,
+        required:[true,"this field is required"]
     },
     auteur:{
-        // chaine de caractère avec la valeur "unkown" par defaut
+        type:String
     },
     resume:{
-        // chaine de caractère non nulle de moins de 100 caractères
-    },
+        type:String,
+        required:[true,"this field is required"],
+        maxLength:100
+    }, 
 
     content: {
-        // chaine de caractère non nulle, au moins de 100 caractères
+        type:String,
+        required:[true,"this field is required"],
+        minLength:100
     }
 }, {timestamps:true})
 
 //créer et exporter un Modéle Post
+const post=mongoose.model("post",postSchema);
+module.exports=post;
